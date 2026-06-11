920aa25b9b724b696c549d2a07dc047e7a1efb39
Cybergman-droid <jasongakpo20@gmail.com>
2026-06-11 23:21:43 +0100

    refactor: wrap NavbarButton components with Link for navigation

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,9 +1,11 @@
 import type { ReactElement, ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 type ButtonProps = {
 	children: ReactNode;
 };
 
+// Navbar button that is rendered in the navbar
 function NavbarButton({ children }: ButtonProps): ReactElement {
 	return (
 		<button className='btn btn-outline btn-secondary rounded-3xl'>
@@ -12,16 +14,31 @@ function NavbarButton({ children }: ButtonProps): ReactElement {
 	);
 }
 
+// Navbar element that is rendered at the top of every page
 function Navbar() {
 	return (
 		<nav className='flex gap-10 justify-center'>
-			<NavbarButton>Dashboard</NavbarButton>
-			<NavbarButton>Charts</NavbarButton>
-			<NavbarButton>File upload</NavbarButton>
-			<NavbarButton>Budgets</NavbarButton>
-			<NavbarButton>Transactions</NavbarButton>
-			<NavbarButton>Calculators</NavbarButton>
+			<Link to='/'>
+				<NavbarButton>Dashboard</NavbarButton>
+			</Link>
+			<Link to='/Charts'>
+				<NavbarButton>Charts</NavbarButton>
+			</Link>
+			<Link to='/Import'>
+				<NavbarButton>File upload</NavbarButton>
+			</Link>
+			<Link to='/Budgets'>
+				<NavbarButton>Budgets</NavbarButton>
+			</Link>
+			<Link to='/Transactions'>
+				<NavbarButton>Transactions</NavbarButton>
+			</Link>
+			<Link to='/Calculators'>
+				<NavbarButton>Calculators</NavbarButton>
+			</Link>
 		</nav>
 	);
 }
+
+// Exports the Navbar component so that it can be used in other places
 export default Navbar;
