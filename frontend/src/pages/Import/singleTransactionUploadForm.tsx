43f6d424fd2ef2502450824453d2d0fb04424ba2
Cybergman-droid <jasongakpo20@gmail.com>
2026-09-04 17:46:58 +0100

    feat: implement form submission handling in SingleTransactionUploadForm and restructure form layout

--- frontend/src/pages/Import/singleTransactionUploadForm.tsx
+++ frontend/src/pages/Import/singleTransactionUploadForm.tsx
@@ -3,6 +3,16 @@ import NumberInputField from "../../components/form/inputFieldNumber.tsx";
 import DropdownMenu from "../../components/form/dropdown.tsx";
 import SubmitButton from "../../components/form/submitButton.tsx";
 import DateInputField from "../../components/form/inputFieldDate.tsx";
+import type { FormEvent } from "react";
+
+type Transaction = {
+	transactionName: string;
+	transactionDescription: string;
+	transactionType: "Deposit" | "Withdrawal";
+	transactionAmount: number;
+	transactionCategory: string;
+	transactionDate: Date;
+};
 
 const transactionTypeOptions = ["Deposit", "Withdrawal"];
 const categoryDropdownOptions = [
@@ -19,37 +29,54 @@ const categoryDropdownOptions = [
 ];
 
 function SingleTransactionUploadForm() {
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const transactionFormData = new FormData(e.currentTarget);
+		const payload = Object.fromEntries(transactionFormData);
+
+		console.log(payload);
+	};
+
 	return (
-		<form>
+		<>
 			<p className='justify-self-center text-3xl font-bold mb-15'>
 				Enter the information for a single transaction
 			</p>
-			<div className='flex flex-col gap-6'>
-				<TextInputField placeholder='Transaction Name' name='transactionName' />
-				<TextInputField
-					placeholder='Transaction Description (optional)'
-					variant='transactionDescription'
-					name='transactionDescription'
-				/>
-				<div className='flex justify-between gap-6'>
-					<DropdownMenu
-						defaultValue='Deposit or Withdrawal'
-						dropdownOptions={transactionTypeOptions}
-						name='transactionType'
+
+			<form onSubmit={handleSubmit}>
+				<div className='flex flex-col gap-6'>
+					<TextInputField
+						placeholder='Transaction Name'
+						name='transactionName'
 					/>
-					<NumberInputField placeholder='Amount' name='transactionAmount' />
-				</div>
-				<div className='flex justify-between gap-6'>
-					<DropdownMenu
-						defaultValue={"Categories"}
-						dropdownOptions={categoryDropdownOptions}
-						name='transactionCategory'
+					<TextInputField
+						placeholder='Transaction Description (optional)'
+						variant='transactionDescription'
+						name='transactionDescription'
 					/>
-					<DateInputField name='transactionDate' />
+
+					<div className='flex justify-between gap-6'>
+						<DropdownMenu
+							defaultValue='Deposit or Withdrawal'
+							dropdownOptions={transactionTypeOptions}
+							name='transactionType'
+						/>
+						<NumberInputField placeholder='Amount' name='transactionAmount' />
+					</div>
+
+					<div className='flex justify-between gap-6'>
+						<DropdownMenu
+							defaultValue={"Categories"}
+							dropdownOptions={categoryDropdownOptions}
+							name='transactionCategory'
+						/>
+						<DateInputField name='transactionDate' />
+					</div>
+
+					<SubmitButton name='transactionSubmit' />
 				</div>
-				<SubmitButton name='transactionSubmit' />
-			</div>
-		</form>
+			</form>
+		</>
 	);
 }
 
