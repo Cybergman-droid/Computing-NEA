4a69115236cc23210a5dd5d2bfda1974db210093
Cybergman-droid <jasongakpo20@gmail.com>
2026-09-12 02:35:35 +0100

    feat: enhance CSV import functionality with Papa Parse and improved error handling

--- backend/routes/importRoutes.ts
+++ backend/routes/importRoutes.ts
@@ -1,43 +1,144 @@
 import { Database } from "better-sqlite3";
 import { Router } from "express";
 import multer from "multer";
+import Papa from "papaparse";
 
-const uploads = multer();
+type BarclaysTransaction = {
+	"Transaction Date": string;
+	Narrative: string;
+	Debit: number | null;
+	Credit: number | null;
+	Balance: number;
+};
+type ClassifiedTransaction = {
+	date: string;
+	description: string;
+	category: string | null;
+	amount: number;
+	confidence: number | null;
+	autoClassified: boolean;
+	bank: string;
+};
+type NormalizedTransaction = {
+	date: string;
+	description: string;
+	amount: number;
+	bank: string;
+};
+
+const uploads = multer({ storage: multer.memoryStorage() });
+
+function toIsoDate(dateText: string): string {
+	const [day, month, year] = dateText.split("/").map(Number);
+
+	return new Date(Date.UTC(year, month - 1, day)).toISOString();
+}
+
+// Validates and parses the barclays data into a normalized format for use in other parts of the app
+function barclaysParser(
+	parsedCsvArray: BarclaysTransaction[],
+): [NormalizedTransaction[], number, string] {
+	let normalizedTransactions: NormalizedTransaction[] = [];
+	let numRowsSkipped = 0;
+	for (let transaction of parsedCsvArray) {
+		let date = transaction["Transaction Date"];
+		let description = transaction.Narrative;
+		let debit = transaction.Debit;
+		let credit = transaction.Credit;
+
+		if (!date) {
+			numRowsSkipped += 1;
+			continue;
+		}
+		if (!description) {
+			numRowsSkipped += 1;
+			continue;
+		}
+		if (
+			(debit === null && credit === null) ||
+			(debit !== null && !Number.isFinite(debit)) ||
+			(credit !== null && !Number.isFinite(credit))
+		) {
+			numRowsSkipped += 1;
+			continue;
+		}
+
+		const isoDate = toIsoDate(date);
+		let normalizedTransaction: NormalizedTransaction = {
+			description: description,
+			bank: "Barclays",
+			amount: debit !== null ? -debit : credit!,
+			date: isoDate.split("T")[0],
+		};
+
+		normalizedTransactions.push(normalizedTransaction);
+	}
+
+	const dateOfImport = new Date().toISOString();
+	return [normalizedTransactions, numRowsSkipped, dateOfImport];
+}
 
 export default function createImportRoutes(db: Database) {
 	const importRouter = Router();
 
 	// POST route to recieve the CSV file sent to the database using the multer middleware
 	importRouter.post("/", uploads.single("bankCsvFile"), (request, response) => {
-		if (!request.file) {
+		const file = request.file;
+
+		// Returns an error if the file does not exist
+		if (!file) {
 			return response.status(400).json({
 				message: "Please upload a CSV file.",
 			});
 		}
 
-		console.log("Original name:", request.file?.originalname);
-		console.log("MIME type:", request.file?.mimetype);
-		console.log("Size:", request.file?.size);
+		// Converts the CSV file into a string of text for parsing
+		const csvFileText = file.buffer.toString("utf-8");
 
-		// If there are no errors the transaction is inserted into the database
-		const importLogInsertStatement = `
+		// Parses the CSV file into an array of JSON bodies using Papa parse
+		Papa.parse<BarclaysTransaction>(csvFileText, {
+			header: true,
+			skipEmptyLines: true,
+			dynamicTyping: true,
+			complete: (results) => {
+				const parsedCsv = results.data;
+
+				const [parsedTransactions, numRowsSkipped, dateOfImport] =
+					barclaysParser(parsedCsv);
+				const numRowsImported = parsedTransactions.length;
+
+				console.log("Parsed Transactions:", parsedTransactions);
+				console.log("number of rows skipped", numRowsSkipped);
+				console.log("number of rows imported", numRowsImported);
+
+				// If there are no errors the transaction summary is inserted into the database
+				const importLogInsertStatement = `
                 INSERT INTO import_log
                 (date,filename,imported,skipped,bank)
                 VALUES (?, ?, ?, ?, ?)
             `;
-		const result = db
-			.prepare(importLogInsertStatement)
-			.run("2025-09-5", "monzo-may-2025.csv", 47, 3, "Monzo");
-		response.status(201).json({
-			message: "File uploaded successfully.",
-			filename: request.file.originalname,
+				const result = db
+					.prepare(importLogInsertStatement)
+					.run(
+						dateOfImport.split("T")[0],
+						file?.originalname,
+						numRowsImported,
+						numRowsSkipped,
+						parsedTransactions[0].bank,
+					);
+
+				response.status(201).json({
+					message: "File uploaded successfully.",
+					filename: file.originalname,
+				});
+				console.log(result);
+			},
+			// TODO add the classifier class and function
 		});
-		console.log(result);
 	});
 
 	// GET route to send the import log data to the frontend
 	importRouter.get("/", (request, response) => {
-		console.log(request);
 		try {
 			// Select the most recent import log from the import_log table
 			const importLogSelectStatement = `SELECT * FROM import_log ORDER BY id DESC LIMIT 1`;
