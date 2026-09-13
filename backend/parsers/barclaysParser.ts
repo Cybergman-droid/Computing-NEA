0ecbaa28d4cd34268640ca46b23ab105861415a8
Cybergman-droid <jasongakpo20@gmail.com>
2026-09-13 02:35:55 +0100

    feat: improve code readability by adding comments and formatting in barclaysParser

--- backend/parsers/barclaysParser.ts
+++ backend/parsers/barclaysParser.ts
@@ -2,6 +2,7 @@ import {
 	BarclaysTransaction,
 	NormalizedTransaction,
 } from "../types/Transaction";
+
 function amountParser(credit: number | null, debit: number | null): number {
 	if (credit === null && debit === null) {
 		throw new Error("Either credit or debit is required");
@@ -31,9 +32,9 @@ export default function barclaysParser(
 		let credit = transaction.Credit;
 		let isoDate;
 
+		// Validates the date and converts into a date object
 		try {
 			const [day, month, year] = date.split("/").map(Number);
-
 			isoDate = new Date(Date.UTC(year, month - 1, day)).toISOString();
 		} catch (error) {
 			numRowsSkipped += 1;
@@ -44,6 +45,8 @@ export default function barclaysParser(
 			numRowsSkipped += 1;
 			continue;
 		}
+
+		// Checks that only one of credit or debit is a number with the other being null
 		if (
 			(debit === null && credit === null) ||
 			(credit !== null && debit !== null) ||
